d2ae4be2f0e8edf28146b18e9bf95a1d57cf5966
Hasnain-Khattak <msaqibkhan987987@gmail.com>
2024-09-01 15:57:01 +0500

    Updated

--- Python_Script/script.py
+++ Python_Script/script.py
@@ -19,16 +19,65 @@ files = glob('../Raw/MetaMotion/*.csv')
 # Extract features from filename
 # --------------------------------------------------------------
 
-data_path = '../Raw/MetaMotion/*.csv'
+data_path = '../Raw/MetaMotion\\'
+
+f = files[0]
+### We are fetching three things from file path
+# 1. Participant (A, B, C, D, E)
+participant = f.split('-')[0]
+participant= participant.replace(data_path, "")
+
+# 2. Label 
+label = f.split('-')[1]
+
+# 3. Category
+category = f.split('-')[2].rstrip('1234')
+
+## Creating a DataFrame
+df = pd.read_csv(f)
+df['Participant']  = participant
+df['label'] = label
+df['category'] = category
 
 # --------------------------------------------------------------
 # Read all files
 # --------------------------------------------------------------
 
+### Creating an Empty DataFrame
+
+## acc stands for Accelerometer DataFrame
+acc_df = pd.DataFrame()
+
+###gyro stands for gyroscope DataFrame
+gyro_df = pd.DataFrame()
+
+## Creating a Counter
+acc_set = 1
+gyro_set = 1
+
+for file in files:
+    participant = file.split('-')[0]
+    participant= participant.replace(data_path, "")
+    label = file.split('-')[1]
+    category = file.split('-')[2].rstrip('123').rstrip('2_MetaWear_2019')
+    df = pd.read_csv(file)
+    df['Participant']  = participant
+    df['label'] = label
+    df['category'] = category 
+    if 'Accelerometer' in file:
+        df['set'] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
+    if 'Gyroscope' in file:
+        df['set'] = gyro_set
+        gyro_set += 1
+        gyro_df = pd.concat([gyro_df, df])
 
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+
+
 
 
 # --------------------------------------------------------------
